d133e1c075d42c606e1ba3cc0547587c50954713
maxbauerw@gmail.com <maxbauerw@gmail.com>
2025-09-17 16:10:51 -0400

    added mobile func on portfolio. fixed grid layout on portfolio

--- src/components/GridBox.jsx
+++ src/components/GridBox.jsx
@@ -1,27 +1,38 @@
 'use client'
 
+import { useState } from 'react'
 
-const GridBox = ({thumb, title, description, category, className, titleStyle="text-sm 2xs:text-lg xs:!text-xl", children} ) => {
+const GridBox = ({ thumb, title, description, category, className, titleStyle = "text-sm 2xs:text-lg xs:!text-xl", children }) => {
+  const [isContentVisible, setIsContentVisible] = useState(false)
+
+  const handleClick = () => {
+    setIsContentVisible(!isContentVisible)
+  }
 
   return (
-    <div className="relative  aspect-square bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 cursor-pointer group">
+    <div
+      className="relative aspect-square bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 cursor-pointer group"
+      onClick={handleClick}
+    >
       <div className={`w-full h-[70%] bg-contain bg-center bg-no-repeat relative ${thumb} ${className}`}>
-          <div className="absolute inset-0 bg-black/90 bg-opacity-40 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <div className="h-1/2 flex flex-col justify-end items-center pb-5">
-                {children}
-              </div>
-              <div className="h-1/2 flex flex-col justify-start items-center pt-2">
-              <p className="opacity-100 text-white px-8 text-align-center"> {description} </p>
-              </div>
+        <div
+          id="gb-hidden-content"
+          className={`absolute inset-0 bg-black/90 bg-opacity-40 flex flex-col items-center justify-center transition-opacity duration-300 ${isContentVisible ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'}`}
+        >
+          <div className="h-1/2 flex flex-col justify-end items-center pb-5">
+            {children}
           </div>
+          <div className="h-1/2 flex flex-col justify-start items-center pt-2">
+            <p className="opacity-100 text-white px-8 text-center"> {description} </p>
+          </div>
+        </div>
       </div>
       <div className="p-5 h-[30%] flex flex-col justify-center bg-gray-300/50">
-          <h3 className={`font-semibold text-slate-700 mb-1 ${titleStyle}`}>{title}</h3>
-          <p className={`text-[3vw] xs:!text-sm text-gray-500 uppercase tracking-wider `}>{category}  </p>
+        <h3 className={`font-semibold text-slate-700 mb-1 ${titleStyle}`}>{title}</h3>
+        <p className={`text-[3vw] xs:!text-sm text-gray-500 uppercase tracking-wider `}>{category}</p>
       </div>
-  </div>
- )
+    </div>
+  )
 }
 
 export default GridBox;
-
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -73,7 +73,7 @@ function Portfolio({
   const portLink = (<a href="#" className="bg-white bg-opacity-90 text-slate-700 px-5 py-2.5 rounded-full font-medium no-underline hover:bg-white transition-colors duration-300">View Project</a>)
   
   const gdplink = (<Link href="/gdp" className="bg-white bg-opacity-90 text-slate-700 px-5 py-2.5 rounded-full font-medium no-underline hover:bg-white transition-colors duration-300">View Project</Link>);
-  const playuplink = (<a href="https://github.com/mbauerw/playup" className="bg-white bg-opacity-90 text-slate-700 px-5 py-2.5 rounded-full font-medium no-underline hover:bg-white transition-colors duration-300">View Project</a>);
+  const playuplink = (<a href="https://github.com/mbauerw/playup-next" className="bg-white bg-opacity-90 text-slate-700 px-5 py-2.5 rounded-full font-medium no-underline hover:bg-white transition-colors duration-300">View Project</a>);
   const elfflink = (<a href="https://elff.com/" className="bg-white bg-opacity-90 text-slate-700 px-5 py-2.5 rounded-full font-medium no-underline hover:bg-white transition-colors duration-300">View Project</a>);
   const dadlink = (<a href="https://johannesmbauer.com/" className="bg-white bg-opacity-90 text-slate-700 px-5 py-2.5 rounded-full font-medium no-underline hover:bg-white transition-colors duration-300">View Project</a>);
   const gitlink = (<a href="https://github.com/mbauerw" className="bg-white bg-opacity-90 text-slate-700 px-5 py-2.5 rounded-full font-medium no-underline hover:bg-white transition-colors duration-300">View Project</a>);
@@ -100,7 +100,7 @@ function Portfolio({
           <p className="text-lg a6  text-stone-100">Explore my latest projects and creative work</p>
         </div>
 
-        <div className="grid grid-cols-[repeat(auto-fit,minmax(1fr,1fr))]  xs:grid-cols-[repeat(auto-fit,minmax(300px,1fr))] md:grid-rows-[repeat(auto-fit,minmax(350px,1fr))] gap-4 sm:gap-6 md:gap-10 py-3 pt-10">
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(1fr,1fr))]  xs:grid-cols-[repeat(auto-fit,minmax(300px,1fr))] md:grid-cols-[repeat(auto-fit,minmax(350px,1fr))]  gap-4 sm:gap-6 md:gap-10 py-3 pt-10">
          
           <GridBox thumb={thumbs[0]} title={"The Impact of Economic Growth on Population Change"} category={"Data Analysis"} description={"Data Analysis project exploring the relationships between economic growth and population change across the United States"} titleStyle={"text-sm 2xs:text-md xs:!text-lg"} className={"bg-cover"} children={gdplink}></GridBox>
  
